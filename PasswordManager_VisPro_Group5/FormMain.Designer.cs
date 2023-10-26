4f1230ed3cc89a57df74cf08079212b4995522da
Krismarvel Ticoalu <avelticoalu22@gmail.com>
2023-10-26 15:44:37 +0800

    koneksi ke database

--- PasswordManager_VisPro_Group5/FormMain.Designer.cs
+++ PasswordManager_VisPro_Group5/FormMain.Designer.cs
@@ -28,18 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.newPasswordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.passwordList = new System.Windows.Forms.DataGridView();
-            this.title = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.username_email = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.password = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.deleteRow = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.passwordList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -61,85 +54,21 @@
             this.newPasswordToolStripMenuItem.Text = "Create password";
             this.newPasswordToolStripMenuItem.Click += new System.EventHandler(this.newPasswordToolStripMenuItem_Click);
             // 
-            // passwordList
+            // dataGridView1
             // 
-            this.passwordList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
-            this.passwordList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.passwordList.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.passwordList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.passwordList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.passwordList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.title,
-            this.username_email,
-            this.password});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.passwordList.DefaultCellStyle = dataGridViewCellStyle2;
-            this.passwordList.Location = new System.Drawing.Point(26, 50);
-            this.passwordList.Name = "passwordList";
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.passwordList.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            this.passwordList.Size = new System.Drawing.Size(603, 362);
-            this.passwordList.TabIndex = 3;
-            this.passwordList.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
-            // 
-            // title
-            // 
-            this.title.FillWeight = 59.07928F;
-            this.title.HeaderText = "          Title          ";
-            this.title.Name = "title";
-            this.title.Width = 197;
-            // 
-            // username_email
-            // 
-            this.username_email.FillWeight = 159.0576F;
-            this.username_email.HeaderText = "Username/Email";
-            this.username_email.Name = "username_email";
-            this.username_email.Width = 189;
-            // 
-            // password
-            // 
-            this.password.FillWeight = 81.86312F;
-            this.password.HeaderText = "    Password    ";
-            this.password.Name = "password";
-            this.password.Width = 172;
-            // 
-            // deleteRow
-            // 
-            this.deleteRow.Font = new System.Drawing.Font("Nirmala UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.deleteRow.Location = new System.Drawing.Point(265, 434);
-            this.deleteRow.Name = "deleteRow";
-            this.deleteRow.Size = new System.Drawing.Size(115, 50);
-            this.deleteRow.TabIndex = 7;
-            this.deleteRow.Text = "Delete";
-            this.deleteRow.UseVisualStyleBackColor = true;
-            this.deleteRow.Click += new System.EventHandler(this.button1_Click);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(596, 382);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(656, 496);
-            this.Controls.Add(this.deleteRow);
-            this.Controls.Add(this.passwordList);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
@@ -149,7 +78,7 @@
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.passwordList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,10 +88,6 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem newPasswordToolStripMenuItem;
-        private System.Windows.Forms.DataGridView passwordList;
-        private System.Windows.Forms.DataGridViewTextBoxColumn title;
-        private System.Windows.Forms.DataGridViewTextBoxColumn username_email;
-        private System.Windows.Forms.DataGridViewTextBoxColumn password;
-        private System.Windows.Forms.Button deleteRow;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- PasswordManager_VisPro_Group5/FormMain.cs
+++ PasswordManager_VisPro_Group5/FormMain.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace PasswordManager_VisPro_Group5
 {
     public partial class FormMain : Form
     {
+        private MySqlConnection koneksi;
+        private MySqlDataAdapter adapter;
+        private MySqlCommand perintah;
+
+        private DataSet ds = new DataSet();
+        private string alamat, query;
         public FormMain()
         {
+            alamat = "server=localhost; database=db_password; username=root; password=;";
+            koneksi = new MySqlConnection(alamat);
             InitializeComponent();
         }
 
@@ -21,15 +33,6 @@ namespace PasswordManager_VisPro_Group5
         {
             FormGenerator formGenerator = new FormGenerator();
             formGenerator.Show();
-
-            // Create a task that will delay for 10 seconds
-            Task.Delay(TimeSpan.FromSeconds(2))
-                // Continue with a statement that will print "Hello" to the console
-                .ContinueWith(t => passwordList.Rows[0].Cells["Password"].Value = "r3W@47V78M");
-
-            
-
-            
         }
 
 
@@ -45,15 +48,39 @@ namespace PasswordManager_VisPro_Group5
 
         private void FormMain_Load(object sender, EventArgs e)
         {
+            try
+            {
+                koneksi.Open();
+                query = string.Format("Select * from tbl_item");
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                ds.Clear();
+                adapter.Fill(ds);
+                koneksi.Close();
 
+                dataGridView1.DataSource = ds.Tables[0];
+                dataGridView1.Columns[0].Width = 200;
+                dataGridView1.Columns[0].HeaderText = "Title";
+                dataGridView1.Columns[1].Width = 200;
+                dataGridView1.Columns[1].HeaderText = "Username/Email";
+                dataGridView1.Columns[2].Width = 200;
+                dataGridView1.Columns[2].HeaderText = "Password";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            passwordList.Rows[0].Cells[0].Value = "";
-            passwordList.Rows[0].Cells[1].Value = "";
-            passwordList.Rows[0].Cells[2].Value = "";
+           
         }
     }
 }
