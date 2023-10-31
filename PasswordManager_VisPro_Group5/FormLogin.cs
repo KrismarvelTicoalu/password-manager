de9c9d2f4dcc03eb47ef8aca3157fb8db9b172a3
Krismarvel Ticoalu <avelticoalu22@gmail.com>
2023-10-31 12:44:13 +0800

    add login feature

--- PasswordManager_VisPro_Group5/FormLogin.cs
+++ PasswordManager_VisPro_Group5/FormLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,13 +8,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 
 namespace PasswordManager_VisPro_Group5
 {
     public partial class FormLogin : Form
     {
+        private MySqlConnection koneksi;
+        private MySqlDataAdapter adapter;
+        private MySqlCommand perintah;
+
+        private DataSet ds = new DataSet();
+        private string alamat, query;
         public FormLogin()
         {
+            alamat = "server=localhost; database=db_password; username=root; password=;";
+            koneksi = new MySqlConnection(alamat);
+
             InitializeComponent();
         }
 
@@ -24,38 +37,45 @@ namespace PasswordManager_VisPro_Group5
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (txtUsernameOrEmail.Text == "")
+            try
             {
-                if (txtPassword.Text == "")
+                query = string.Format("select * from tbl_user where `Username/Email` = '{0}' and Password = '{1}'", txtUsernameOrEmail.Text, txtPassword.Text);
+                ds.Clear();
+                koneksi.Open();
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                adapter.Fill(ds);
+                koneksi.Close();
+                if (ds.Tables[0].Rows.Count > 0)
                 {
-                    FormMain formMain = new FormMain();
-                    formMain.Show();
-                    this.Hide();
+                    foreach (DataRow kolom in ds.Tables[0].Rows)
+                    {
+                        string sandi, namaPengguna;
+                        namaPengguna = kolom["Username/Email"].ToString();
+                        sandi = kolom["Password"].ToString();
+                        if (sandi == txtPassword.Text && namaPengguna == txtUsernameOrEmail.Text)
+                        {
+                            FormMain formMain = new FormMain();
+                            formMain.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Anda salah input password");
+                        }
+                    }
+
                 }
                 else
                 {
-                    MessageBox.Show("Wrong password");
+                    MessageBox.Show("Username not found, please sign up first");
                 }
             }
-            else if (txtPassword.Text == "")
+            catch (Exception ex)
             {
-                if (txtUsernameOrEmail.Text == "")
-                {
-                    FormMain formMain = new FormMain();
-                    formMain.Show();
-                    this.Hide();
-                }
-                else
-                {
-                    MessageBox.Show("Wrong username or email");
-                }
+                MessageBox.Show(ex.ToString());
             }
-            else
-            {
-                MessageBox.Show("Wrong username/email and password");
-            }
-            
-            
         }
 
         private void FormLogin_Load(object sender, EventArgs e)
